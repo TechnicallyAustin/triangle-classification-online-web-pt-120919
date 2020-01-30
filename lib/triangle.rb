422c0ba320113bad01b9742d426aff8d21326c63
Austin Johnson <technicallyaustin@gmail.com>
2020-01-30 22:19:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,39 +15,8 @@ def initialize(l1, l2, l3)
 @isosceles = isosceles
  end
  
+ def valid?
+ end
  
-def valid?
-# The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality. Further, each side must be larger than 0.
-length3 = @tri_l[0] + @tri=[1]
-length1 = @tri_l[1] + @tri_l[2] 
-length2 = @tri_l[2] + @tri_l[0]
-
-if @tri_l.none? { |i| i <= 0 } && @l3 > length3
-  true 
-end 
-end
- 
- 
- def kind
-    equilateral = @tri_l.all? { |i| i == @l1 && @l1 == @l2 && @l2 == @l3 }
-    
-    isosceles = @tri_l.each { |i| (i == @l1 && @l1 == @l2) || (i == @l2 && @l2 == @l3) || (i == @l2 && @l2 == @l1 )}
-    
-    scalene = @tri_l.each { |i| (i == @l1) && (@l2 != @l3) && (@l1 != @l2)}
-    if equilateral
-      :equilateral 
-      elsif isosceles
-      :isosceles
-      elsif scalene
-        :scalene
-end
-end
-
-
-
-
-
-
-
-  
-end #class end 
+ def kind 
+ end
